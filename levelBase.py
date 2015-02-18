2155520661fac231cf1c8c9268ea3b10c01c220c
Neill Henning <neillhenning@gmail.com>
2015-02-18 17:48:48 +0200

    add initial level

---
+++ levelBase.py
@@ -0,0 +1,15 @@
+class Level:
+    """The Base Class for Levels"""
+    def getLayout(self):
+        """
+        Get the Layout of the Level 
+        Returns a [][] list
+        """
+        pass
+    def getImages(self):
+        """
+        Get a list of all the images used by the level
+        Returns a list of all the images used. The indices in the layout refer
+        to sprites in the list return by this funtion
+        """
+        pass
